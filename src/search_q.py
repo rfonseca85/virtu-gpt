058065975d469e80f441cd2af9fa3f69f0a7392a
Rafael Fonseca <rfonseca85@yahoo.ca>
2023-12-04 14:46:31 -0500

    Dev (#12)
    * Added a cache to the load models function, made it a lot faster to load the pages :)
    
    * Added a cache to the load models function, made it a lot faster to load the pages :)

--- src/search_q.py
+++ src/search_q.py
@@ -8,6 +8,7 @@ import chromadb
 import time
 from constants import CHROMA_SETTINGS
 import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
 
 # Flags
 mute_stream = False
@@ -34,27 +35,35 @@ def load_model():
 
     callbacks = [] if mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
-    match model_type:
-        case "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks,
-                           verbose=False)
-        case "GPT4All":
-            llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gguf', n_batch=model_n_batch,
-                          callbacks=callbacks, verbose=False)
-        case _default:
-            # raise exception if model_type is not supported
-            raise Exception(
-                f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+    llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gguf', n_batch=model_n_batch,
+                  callbacks=callbacks, verbose=False)
 
-    return [llm, retriever]
+    # Get the answer from the chain
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever,
+                                     return_source_documents=True)
+
+    # match model_type:
+    #     case "LlamaCpp":
+    #         llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks,
+    #                        verbose=False)
+    #     case "GPT4All":
+    #         llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gguf', n_batch=model_n_batch,
+    #                       callbacks=callbacks, verbose=False)
+    #     case _default:
+    #         # raise exception if model_type is not supported
+    #         raise Exception(
+    #             f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+
+    return qa
 
 
 def main():
-    parameters = load_model()
+    qa = load_model()
     response = None
 
     with st.sidebar:
         st.title("Search configs")
+        global show_source
         show_source = st.checkbox("Show source documents", value=True)
 
     st.title("💬 " + config('COMPANY_NAME') + " Q")
@@ -73,41 +82,31 @@ def main():
         # Start the response time
         start_time = time.time()
 
-        # Get the answer from the chain
-        qa = RetrievalQA.from_chain_type(llm=parameters[0], chain_type="stuff", retriever=parameters[1],
-                                         return_source_documents=True)
-        response = qa(prompt)
-        msg = response["result"]
-        st.session_state.messages.append({"role": "assistant", "content": msg})
-        st.chat_message("assistant").write(msg)
+        with st.spinner('Wait for it...'):
+            response = qa(prompt)
+            msg = response["result"]
+            st.session_state.messages.append({"role": "assistant", "content": msg})
+            st.chat_message("assistant").write(msg)
 
-        # Start the response time
-        end_time = time.time()
+            # Start the response time
+            end_time = time.time()
 
-        st.caption("Search took - {:.2f} seconds".format(end_time - start_time))
-
-        if show_source:
-            # Custom CSS to inject for prettier expander
-            st.markdown("""
-                <style>
-                div.st-Expander > div:first-child {
-                    font-size: 16px;
-                    font-weight: bold;
-                    color: #0c6ef7;
-                    background-color: #e6f0ff;
-                    border-radius: 5px;
-                    padding: 5px 10px;
-                }
-                div.st-Expander > div:last-child {
-                    background-color: #f2f2f2;
-                    padding: 10px;
-                    border-radius: 5px;
-                    margin-top: 5px;
-                }
-                </style>
-                """, unsafe_allow_html=True)
-
-            # Display documents in expandable sections
-            for i, document in enumerate(response['source_documents'], start=1):
-                with st.expander(f"Document {i}: {document.metadata['source']}"):
-                    st.markdown("**Content:**\n" + document.page_content)
+            if show_source:
+                with stylable_container(
+                        key="container_with_border1",
+                        css_styles="""
+                            {
+                                border: 2px solid rgba(49, 51, 63, 0.2);
+                                border-radius: 0.5rem;
+                                padding: calc(1em - 1px);
+                                background-color: #F0F0F0;
+                            }
+                            .stMarkdown {
+                                color: #000000;
+                            }
+                            """,
+                ):
+                    st.caption("Search took - {:.2f} seconds".format(end_time - start_time))
+                    for i, document in enumerate(response['source_documents'], start=0):
+                        st.caption(f"**Document {i}: {document.metadata['source']}**")
+                        st.caption("**Content:**\n" + document.page_content)
