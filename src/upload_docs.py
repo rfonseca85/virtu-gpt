05df773fc5bfc39f0f89fce7c9df16e7d6ba89f8
rafael.fonseca <rfonseca85@yahoo.ca>
2023-12-04 21:42:37 -0500

    Fixed uploading for docs

--- src/upload_docs.py
+++ src/upload_docs.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import scripts.ingest_utils as ingest
+import tempfile
+import os
 
 
 def main():
-    st.file_uploader("Upload file here")
+    # Streamlit app code
+    uploaded_file = st.file_uploader("Upload Source Document")
+    # uploaded_files = st.file_uploader("Upload Documents", accept_multiple_files=True)
+
+    if uploaded_file is not None:
+        st.caption("You have uploaded the following PDF:")
+        st.caption(uploaded_file.name)
+
+        with st.spinner('Uploading docs...'):
+            # Create a temporary file to store the uploaded PDF
+            temp_file = tempfile.NamedTemporaryFile(suffix=".whatever")
+            temp_file.write(uploaded_file.getvalue())
+
+            # Get the name of the uploaded PDF
+            new_temp_file = os.path.dirname(temp_file.name) + '/' + uploaded_file.name
+            os.rename(temp_file.name, new_temp_file)
+
+            # Ingest the PDF
+            ingest.vector_documents(new_temp_file)
+
+            # Deleting the temporary PDF
+            os.remove(new_temp_file)
