aae839b10a488835e7a248e78865b80ad8cc5e57
Rafael Fonseca <rfonseca85@yahoo.ca>
2023-12-07 11:34:17 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 docker build -t virtu_gpt .
 
 # Step 2: Create Docker volumes for storing models and source documents.
-# These volumes will persist data even after containers are removed.
+# # These volumes will persist data even after containers are removed.
 docker volume create models
 docker volume create source_documents
 
 # Step 3: Run a Docker container from the "virtu_gpt" image in detached mode (-d).
 # Mount the "models" volume to the "/usr/src/app/models" directory inside the container,
 # and mount the "source_documents" volume to the "/usr/src/app/source_documents" directory.
-docker run -d -v models:/usr/src/app/models -v source_documents:/usr/src/app/source_documents virtu_gpt
+docker run --name virtu_gpt -p 8501:8501 -d -v models:/usr/src/app/models -v source_documents:/usr/src/app/source_documents virtu_gpt
 
 # Logging: Print a message indicating the Docker setup is complete.
-echo "Docker setup is complete. Container is running with models and source documents volumes."
+# echo "Docker setup is complete. Container is running with models and source documents volumes."
